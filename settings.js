5b9e134bb4d0e811fce3a8c7b3ad44d1b6ea4d65
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-25 23:01:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
+global.owner = ['918006025825']
+global.premium = ['918006025825']
+global.ownernomer = '918006025825'
+global.ownername = 'Sirius'
 global.botname = 'Cheems Bot MD V2'
-global.footer = 'Xeon Bot Inc.'
+global.footer = 'Sirius Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
 global.region = 'India, NorthEast, Mizoram'
 global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
 global.myweb = 'https://youtu.be/imFIX-Wrt3s'
 global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.author = 'Sirius'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
